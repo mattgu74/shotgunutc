d33c5a469689c2dec0999e6ee19313726bcdae81
Matthieu Guffroy <mattgu74@gmail.com>
2015-04-16 12:33:00 +0200

    wip

--- index.php
+++ index.php
@@ -38,6 +38,7 @@ function checkRight($payutcClient, $user, $app, $fun_check, $fun_id) {
         }
     } else {
         $fundations = $payutcClient->getFundations(array("user"=> $user, "app" => $app));
+        print_r($fundations);
         foreach($fundations as $fundation) {
             if($fundation['fun_id'] == $fun_id) {
                 return true;
